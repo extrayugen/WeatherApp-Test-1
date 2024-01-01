d4e61cd50d4fd8da1f29b58be9074934864978b4
extrayugen <supershicker@gmail.com>
2024-01-01 23:42:51 +0900

    1

--- WeatherApp-Test-1/Controller/ViewController.swift
+++ WeatherApp-Test-1/Controller/ViewController.swift
@@ -1,11 +1,17 @@
+//
+//  ViewController.swift
+//  WeatherApp-Test-1
+//
+//  Created by t2023-m0031 on 1/1/24.
+//
+
 import UIKit
-import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
 
